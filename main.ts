722e3929a9b7cc8f5957d8524d1843f2e96712da
devdong9897 <dev.dong9897@gmail.com>
2026-03-06 20:42:57 +0900

    feat: 함수타입

--- main.ts
+++ main.ts
@@ -11,3 +11,12 @@ let a: number = 2;
 a = 4;
 a = 9.0;
 a = -8.8;
+
+let b: null = null;
+let c: undefined = undefined;
+
+function double(n: number): number {
+  return n * 2;
+}
+
+console.log(double(4));
